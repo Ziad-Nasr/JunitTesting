29f31aec8495400e4f9586b30989a2a2887f348c
Ziad-Nasr <ziadnasr988@gmail.com>
2022-04-22 13:05:07 +0200

    Problem Solve v2

--- Problem1.java
+++ Problem1.java
@@ -3,9 +3,13 @@ public class Problem1 {
  
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
-        int a = input.nextInt();
-        if(a==2) System.out.print("No");
-        else if(a%2==0) System.out.print("Yes");
-        else System.out.print("No");
+        int Inputweight = input.nextInt();
+        System.out.print(EvenWatermelon(Inputweight));
+    }
+    public static String EvenWatermelon (int weight){
+        if(weight==2) System.out.print("No");
+        else if(weight%2==0) return "Yes";
+        else return ("No");
+        return "error";
     }
 }
